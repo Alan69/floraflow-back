0d6a32c25e94b2c905210b164733f4f7dec9b1b7
Alan69 <gameg1676@gmail.com>
2024-12-31 15:03:41 +0500

    orders models

--- stores/views.py
+++ stores/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from .models import Order
+from orders.models import Order
 from .serializers import StoreOrderSerializer, StoreProfileSerializer
 
 # Endpoint to view client orders
