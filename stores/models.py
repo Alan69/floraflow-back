c8c054a566a055c0a13c564de37defe5acdfb902
Alan69 <gameg1676@gmail.com>
2025-01-07 16:15:18 +0500

    store_name added

--- stores/models.py
+++ stores/models.py
@@ -7,6 +7,7 @@ from orders.models import Order
 class StoreProfile(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='store_profile')
+    store_name = models.CharField(max_length=255, blank=True, null=True)
     logo = models.ImageField(upload_to='store_logos/', blank=True, null=True)
     address = models.TextField()
     instagram_link = models.URLField(blank=True, null=True)
