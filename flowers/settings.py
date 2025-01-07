9e7c2279d6d1b6ac481091131b22bf47526c0ff6
Alan69 <gameg1676@gmail.com>
2025-01-07 14:33:50 +0500

    global pagination added

--- flowers/settings.py
+++ flowers/settings.py
@@ -179,7 +179,8 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PERMISSION_CLASSES': [
     #     'rest_framework.permissions.IsAuthenticated',
     # ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'users.pagination.CustomPagination',
     'PAGE_SIZE': 10,  # Number of items per page
 }
 
