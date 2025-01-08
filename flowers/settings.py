1e19aa927a4c460f59dd85be4b41a701a8725f7b
Alan69 <gameg1676@gmail.com>
2025-01-08 16:46:38 +0500

    'django_celery_results' added

--- flowers/settings.py
+++ flowers/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_celery_results',
 
     'rest_framework',
     'rest_framework_simplejwt',
