5efec208bd85ea19f715f2b96b0a36b0c8dfe2b3
Alan69 <gameg1676@gmail.com>
2025-01-15 15:04:55 +0500

    remove landing and bot

--- flowers/settings.py
+++ flowers/settings.py
@@ -78,7 +78,6 @@ INSTALLED_APPS = [
     'stores',
     'orders',
     'api',
-    'landing'
 ]
 
 MIDDLEWARE = [
@@ -164,9 +163,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'landing/static')
-]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
