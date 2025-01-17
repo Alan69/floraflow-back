ccfbd873ef7025e270ac8fe1dccc61125c964273
Alan69 <gameg1676@gmail.com>
2025-01-17 15:59:49 +0500

    admin order new fields added

--- orders/admin.py
+++ orders/admin.py
@@ -4,14 +4,14 @@ from .models import Order, Flower, Color
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     """Admin configuration for the Order model."""
-    list_display = ('uuid', 'client', 'store', 'price', 'status', 'created_at', 'updated_at')
-    list_filter = ('status', 'created_at', 'updated_at')
-    search_fields = ('client__email', 'store__email', 'uuid')
+    list_display = ('uuid', 'client', 'store', 'flower', 'color', 'flower_height', 'quantity', 'price', 'status', 'created_at', 'updated_at')
+    list_filter = ('status', 'flower', 'color', 'created_at', 'updated_at')
+    search_fields = ('client__email', 'store__email', 'uuid', 'flower__text', 'color__text')
     readonly_fields = ('created_at', 'updated_at')
 
     fieldsets = (
         (None, {
-            'fields': ('client', 'store', 'flower_data', 'price', 'status', 'created_at', 'updated_at')
+            'fields': ('client', 'store', 'flower', 'color', 'flower_height', 'quantity', 'city', 'recipients_address', 'recipients_phone', 'flower_data', 'price', 'status', 'created_at', 'updated_at', 'rating')
         }),
     )
 
