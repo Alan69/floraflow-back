d378ca79bbf0f4170e52aeb0d2f644b4d33095d3
Alan69 <gameg1676@gmail.com>
2025-01-28 16:49:12 +0500

    fix

--- payments/serializers.py
+++ payments/serializers.py
@@ -4,5 +4,5 @@ from .models import Tariff
 class TariffSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tariff
-        fields = ['uuid', 'name', 'price', 'days', 'info', 'is_active']
-        read_only_fields = ['uuid', 'name', 'price', 'days', 'info', 'is_active']
+        fields = ['uuid', 'name', 'price', 'info', 'is_active']
+        read_only_fields = ['uuid', 'name', 'price', 'info', 'is_active']
