ede5a9df62c38c4193f5f6a8cdc30ea256fa0e5f
Alan69 <gameg1676@gmail.com>
2025-02-02 17:40:03 +0500

    fix

--- stores/serializers.py
+++ stores/serializers.py
@@ -21,7 +21,13 @@ class StoreProfileSerializer(serializers.ModelSerializer):
         read_only_fields = ['uuid', 'average_rating']
 
 class PriceSerializer(serializers.ModelSerializer):
+    store_name = serializers.CharField(source='order.store.store_profile.store_name', read_only=True)
+    logo = serializers.ImageField(source='order.store.store_profile.logo', read_only=True)
+    instagram_link = serializers.URLField(source='order.store.store_profile.instagram_link', read_only=True)
+    whatsapp_number = serializers.CharField(source='order.store.store_profile.whatsapp_number', read_only=True)
+
     class Meta:
         model = Price
-        fields = ['uuid', 'proposed_price', 'flower_img', 'comment', 'is_accepted', 'expires_at', 'created_at', 'updated_at']
-        read_only_fields = ['uuid', 'is_accepted', 'expires_at', 'created_at', 'updated_at']
+        fields = ['uuid', 'proposed_price', 'flower_img', 'comment', 'is_accepted', 
+                 'created_at', 'updated_at', 'expires_at', 'store_name', 'logo', 
+                 'instagram_link', 'whatsapp_number']
