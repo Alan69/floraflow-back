b6f5dad2473e89ec0f899f0f7a0b724ca79ea814
Alan69 <gameg1676@gmail.com>
2025-02-13 17:34:44 +0500

    fix

--- flowers/celery.py
+++ flowers/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flowers.settings')
@@ -13,6 +14,14 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
+
+# Configure periodic tasks
+app.conf.beat_schedule = {
+    'update-store-ratings-daily': {
+        'task': 'stores.tasks.update_store_ratings',
+        'schedule': crontab(hour=0, minute=0),  # Run at midnight (00:00)
+    },
+}
 
 @app.task(bind=True)
 def debug_task(self):
