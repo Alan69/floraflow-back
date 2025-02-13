cec26ca2587c335f74383a847a6b35406f1f79ee
Alan69 <gameg1676@gmail.com>
2025-02-13 15:51:06 +0500

    fix

--- stores/admin.py
+++ stores/admin.py
@@ -4,14 +4,14 @@ from .models import StoreProfile, Price
 @admin.register(StoreProfile)
 class StoreProfileAdmin(admin.ModelAdmin):
     """Admin configuration for the StoreProfile model."""
-    list_display = ('uuid', 'user', 'address', 'instagram_link')
-    search_fields = ('user__email', 'address', 'instagram_link')
+    list_display = ('uuid', 'user', 'address', 'instagram_link', 'whatsapp_number', 'average_rating')
+    search_fields = ('user__email', 'address', 'instagram_link', 'whatsapp_number')
     list_filter = ('address',)
     readonly_fields = ('uuid',)
 
     fieldsets = (
         (None, {
-            'fields': ('uuid', 'user', 'logo', 'address', 'instagram_link')
+            'fields': ('uuid', 'user', 'logo', 'address', 'instagram_link', 'whatsapp_number', 'average_rating')
         }),
     )
 
