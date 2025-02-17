b235fe0b10df21ea4570bad8051b3e2b6e017932
Alan69 <gameg1676@gmail.com>
2025-02-17 11:58:17 +0500

    fix

--- flowers/settings.py
+++ flowers/settings.py
@@ -180,11 +180,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ],
-    # 'DEFAULT_PERMISSION_CLASSES': [
-    #     'rest_framework.permissions.IsAuthenticated',
-    # ],
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    # 'PAGE_SIZE': 10,  # Number of items per page
 }
 
 SIMPLE_JWT = {
