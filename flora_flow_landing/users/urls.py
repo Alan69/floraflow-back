95f5e18f72be9c9684afb7649c60cbaa765134a1
Alan69 <gameg1676@gmail.com>
2025-02-20 16:46:28 +0500

    fix

--- flora_flow_landing/users/urls.py
+++ flora_flow_landing/users/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
-    path('store/profile/', views.StoreProfileView.as_view(), name='store_profile'),
+    path('store-profile/', views.StoreProfileView.as_view(), name='store_profile_update'),
 ] 
