b9a8cff6118e01d4cf2eeaa81e9652c3b3102c23
Alan69 <gameg1676@gmail.com>
2025-02-21 15:04:36 +0500

    fix

--- stores/consumers.py
+++ stores/consumers.py
@@ -1,24 +1,50 @@
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
+from rest_framework_simplejwt.tokens import AccessToken
+from django.contrib.auth import get_user_model
+from channels.middleware import BaseMiddleware
+from channels.db import database_sync_to_async
+
+User = get_user_model()
+
+@database_sync_to_async
+def get_user_from_token(token):
+    try:
+        access_token = AccessToken(token)
+        user = User.objects.get(id=access_token['user_id'])
+        return user
+    except Exception:
+        return None
 
 class PriceNotificationConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
-        self.user = self.scope["user"]
-        if self.user.is_authenticated:
-            # Add user to a group
-            await self.channel_layer.group_add(f"user_{self.user.id}", self.channel_name)
-            if self.user.user_type == 'store':
-                await self.channel_layer.group_add("store_users", self.channel_name)
-            await self.accept()
-        else:
-            await self.close()
+        # Get token from query string
+        query_string = self.scope.get('query_string', b'').decode()
+        token = None
+        
+        # Try to get token from headers
+        headers = dict(self.scope['headers'])
+        auth_header = headers.get(b'authorization', b'').decode()
+        if auth_header.startswith('Bearer '):
+            token = auth_header.split(' ')[1]
+        
+        if token:
+            user = await get_user_from_token(token)
+            if user:
+                self.user = user
+                # Add user to a group
+                await self.channel_layer.group_add(f"user_{self.user.id}", self.channel_name)
+                if self.user.user_type == 'store':
+                    await self.channel_layer.group_add("store_users", self.channel_name)
+                await self.accept()
+                return
+        
+        await self.close()
 
     async def disconnect(self, close_code):
-        # Remove user from the group
-        if self.user.is_authenticated:
+        if hasattr(self, 'user') and self.user.is_authenticated:
             await self.channel_layer.group_discard(f"user_{self.user.id}", self.channel_name)
             if self.user.user_type == 'store':
                 await self.channel_layer.group_discard("store_users", self.channel_name)
 
     async def send_notification(self, event):
-        # Send data to the WebSocket
         await self.send_json(event["data"])
